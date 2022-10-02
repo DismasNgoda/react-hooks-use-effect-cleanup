9cd79c42bc552b150d37756c7b59588f0238bc48
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-02 09:41:41 +0300

    Completed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   const [showClock, setShowClock] = useState(true);
 
   return (
-    <div>
+    <>
       {showClock ? <Clock /> : null}
       <button onClick={() => setShowClock(!showClock)}>Toggle Clock</button>
-    </div>
+    </>
   );
 }
 
